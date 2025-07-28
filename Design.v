5e54ea0bf53d3bbbd0e899cc39b534f352d33c1f
katuri yeshwanth <katuri.yeswanth@gmail.com>
2025-07-28 16:09:12 +0530

    Update Design.v

--- Design.v
+++ Design.v
@@ -1,3 +1,10 @@
+`timescale 1ns/100ps
+module comparator(x,y,z);
+  input[1:0] x,y;
+  output z;
+  assign z= (x[0]&y[0]&x[1]&y[1]) | (~x[0]&~y[0]&x[1]&y[1]) | (~x[0]&~y[0]&~x[1]&~y[1]) |			(x[0]&y[0]&~x[1]&~y[1]);
+endmodule
+
 module fulladder(a,b,c,sum,cout);
   input a,b,c;
   output sum,cout;
