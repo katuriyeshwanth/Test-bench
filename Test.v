981a27890eaa4072c97170a844ebbd031d873b92
katuri yeshwanth <katuri.yeswanth@gmail.com>
2025-07-28 16:10:53 +0530

    Update Test.v

--- Test.v
+++ Test.v
@@ -1,3 +1,24 @@
+//test bench for comparator
+module test;
+  reg [1:0] x,y;
+  wire z;
+  comparator dut(x,y,z);
+  initial
+    begin
+      $dumpfile ("comparator.vcd");
+      $dumpvars (0, test);
+      x=2'b01;y=2'b00;
+      #10 x=2'b10;y=2'b10;
+      #10 x=2'b01;y=2'b11;
+    end
+  initial
+    begin
+      $monitor("t=%d,x=%2b,y=%2b,z=%d",$time,x,y,z);
+    end
+endmodule
+
+// test bench for full adder
+
 module test;
   reg a,b,c;
   integer i;
