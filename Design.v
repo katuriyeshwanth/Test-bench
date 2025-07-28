f7fc10b7fe948adc828e2f1fac11b1edf79110fc
katuri yeshwanth <katuri.yeswanth@gmail.com>
2025-07-28 16:44:57 +0530

    Update Design.v

--- Design.v
+++ Design.v
@@ -11,3 +11,21 @@ module fulladder(a,b,c,sum,cout);
   assign sum=a^b^c;
   assign cout=(a&b) | (b&c) | (a&c);
 endmodule
+
+module shift_register(clk,clr,a,e);
+  input a,clk,clr;
+  output reg e;
+  reg b,c,d;
+  always @(posedge clk or negedge clr)
+    begin
+      if(!clr)begin b<=0; c<=0; d<=0; e<=0;
+      end
+      else begin
+        
+        e<=d;
+        d<=c;
+        c<=b;
+        b<=a;
+       end
+    end
+endmodule
